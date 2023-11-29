07673e2a34fa7986ae9d4450d413ed48bbaa5566
Junior DUSSOUILLEZ <junior.dussouillez@gmail.com>
2023-11-29 14:17:43 +0100

    chore: remove worker pool execution

--- server/server-rpc/src/main/java/com/github/jdussouillez/server/rpc/service/ProductApiService.java
+++ server/server-rpc/src/main/java/com/github/jdussouillez/server/rpc/service/ProductApiService.java
@@ -9,7 +9,6 @@ import com.github.jdussouillez.server.service.ProductService;
 import com.google.protobuf.Empty;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.infrastructure.Infrastructure;
 import jakarta.inject.Inject;
 
 @GrpcService
@@ -25,7 +24,6 @@ public class ProductApiService implements ProductGrpcApiService {
     @Override
     public Multi<Product> getAll(final Empty request) {
         return productService.getAll()
-            .runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
             .onSubscription()
             .invoke(() -> Loggers.MAIN.info("Sending products..."))
             .onCompletion()
